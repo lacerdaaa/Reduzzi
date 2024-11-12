ecc10b352ae3afbc1abdaf45677ad9a86befe780
lacerdaaa <137107996+lacerdaaa@users.noreply.github.com>
2024-11-12 16:19:09 -0300

    fix: ensure that the indication data is located in firestore && ensure that environment variables is safe

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,15 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getStorage } from "firebase/storage";
 
+import Constants from "expo-constants";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCNVvS3JxzHcfok0x9Y4wLFRZdPiUnN630",
-  authDomain: "reduzzi-6eb49.firebaseapp.com",
-  projectId: "reduzzi-6eb49",
-  storageBucket: "reduzzi-6eb49.firebasestorage.app",
-  messagingSenderId: "217112197945",
-  appId: "1:217112197945:web:0f77a453364359dd6ec6e8",
+  apiKey: Constants.expoConfig.extra.API_KEY,
+  authDomain: Constants.expoConfig.extra.AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,61 +1,48 @@
-// import React, { useEffect, useState } from "react";
-// import AppNavigator from "../navigation/AppNavigator.routes";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { onAuthStateChanged, User } from "firebase/auth";
-// import { Login } from "./pages/auth/login";
-// import { Home } from "./pages/home";
-// import { Wallet } from "./pages/wallet";
-// import { Indication } from "./pages/indication/indicate";
-// import { Profile } from "./pages/profile";
-// import { TrackIndication } from "./pages/indication/trackindicate";
-// import { auth } from "../../firebaseConfig"; 
+import React, { useEffect, useState } from "react";
+import AppNavigator from "../navigation/AppNavigator.routes";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { Login } from "./pages/auth/login";
+import { Home } from "./pages/home";
+import { Wallet } from "./pages/wallet";
+import { Indication } from "./pages/indication/indicate";
+import { Profile } from "./pages/profile";
+import { TrackIndication } from "./pages/indication/trackindicate";
+import { auth } from "../../firebaseConfig"; 
 
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-// function AppTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Home" component={Home} />
-//       <Tab.Screen name="Wallet" component={Wallet} />
-//       <Tab.Screen name="Indicações" component={TrackIndication} />
-//       <Tab.Screen name="Perfil" component={Profile} />
-//     </Tab.Navigator>
-//   );
-// }
-
-// export default function App() {
-//   const [user, setUser] = useState<User | null>(null); // Tipando o estado como User ou null
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//       console.log("Current user:", currentUser); // Depuração para verificar o estado do usuário
-//       if (currentUser) {
-//         setUser(currentUser); // Se houver um usuário autenticado, define o estado
-//       } else {
-//         setUser(null); // Se não houver usuário, define como null
-//       }
-//     });
-
-//     // Remover o listener quando o componente for desmontado
-//     return () => unsubscribe();
-//   }, []);
-
-//   console.log("User state:", user); // Log para depurar o estado do usuário
-
-//   // Remova o NavigationContainer de dentro do componente AppTabs
-//   return user ? <AppTabs /> : <Login />;
-// }
-
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "../navigation/AppNavigator.routes"
-
-const App = () => {
+function AppTabs() {
   return (
-    <SafeAreaProvider>
-      <Navigation /> {/* Usando o componente de navegação */}
-    </SafeAreaProvider>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Wallet" component={Wallet} />
+      <Tab.Screen name="Indicações" component={TrackIndication} />
+      <Tab.Screen name="Perfil" component={Profile} />
+    </Tab.Navigator>
   );
-};
+}
 
-export default App;
+export default function App() {
+  const [user, setUser] = useState<User | null>(null); // Tipando o estado como User ou null
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("Current user:", currentUser); // Depuração para verificar o estado do usuário
+      if (currentUser) {
+        setUser(currentUser); // Se houver um usuário autenticado, define o estado
+      } else {
+        setUser(null); // Se não houver usuário, define como null
+      }
+    });
+
+    // Remover o listener quando o componente for desmontado
+    return () => unsubscribe();
+  }, []);
+
+  console.log("User state:", user); // Log para depurar o estado do usuário
+
+  // Remova o NavigationContainer de dentro do componente AppTabs
+  return user ? <AppTabs /> : <Login />;
+}
+
